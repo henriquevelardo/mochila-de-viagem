8c42c7d91be48c4969246de49e67ba3f04504676
henriquevelardo <henriquevelardo@hotmail.com>
2022-11-23 22:05:09 -0300

    Implementando armazenamento Localstorage

--- mochila-de-viagem/js/main.js
+++ mochila-de-viagem/js/main.js
@@ -1,26 +1,51 @@
 const form = document.getElementById("novoItem")
 const lista = document.getElementById('lista')
+const itens = JSON.parse(localStorage.getItem("itens")) || []
+
+//Cria os elementos apos refresh, mantendo-os
+itens.forEach((elemento) => {
+    criaElemento(elemento)
+})
 
 form.addEventListener("submit", (evento) => {
     evento.preventDefault()
 
+    const nome = evento.target.elements['nome']
+    const quantidade = evento.target.elements['quantidade']
 
-    criaElemento(evento.target.elements['nome'].value, evento.target.elements['quantidade'].value)
+    const itemAtual = {
+        "nome": nome.value,
+        "quantidade": quantidade.value
+    }
+
+    //envia item para ser criado
+    criaElemento(itemAtual)
+    //adiciona item para a lista de itens
+    itens.push(itemAtual)
+    //Salva item no LocalStorage
+    localStorage.setItem("itens", JSON.stringify(itens))
+
+    
+
+    nome.value=""
+    quantidade.value=""
 })
 
-function criaElemento(nome, quantidade){
-    console.log(nome)
-    console.log(quantidade)
-
+function criaElemento(itemAtual){
     /*<li class="item"><strong>7</strong>Camisas</li>*/
     const novoItem = document.createElement('li')
     novoItem.classList.add('item')
 
     const numeroItem = document.createElement('strong')
-    numeroItem.innerHTML = quantidade
+    numeroItem.innerHTML = itemAtual.quantidade
 
     novoItem.appendChild(numeroItem)
-    novoItem.innerHTML += nome
+    novoItem.innerHTML += itemAtual.nome
 
     lista.appendChild(novoItem)
 }
+  
+    
+
+
+    
